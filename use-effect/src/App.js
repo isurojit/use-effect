613f33bad18ed77afdb9c0be338e206115d590f1
isurojit <iamsurojit@hotmail.com>
2022-03-09 20:06:13 +0530

    final commit

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  return <React.Fragment></React.Fragment>;
-}
+const App = () => {
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // const handelResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("resize", handelResize);
+
+  //   return () => {
+  //     window.removeEventListener("resize", handelResize);
+  //   };
+  // }, []);
+
+  const [resourstype, setResourceType] = useState("Posts");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourstype.toLowerCase()}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [resourstype]);
+
+  const postChangeHnadler = () => {
+    setResourceType(() => {
+      return "Posts";
+    });
+  };
+
+  const userChnageHandler = () => {
+    setResourceType(() => {
+      return "Users";
+    });
+  };
+
+  const commentsChangeHandler = () => {
+    setResourceType(() => {
+      return "Comments";
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={postChangeHnadler}>Posts</button>
+      <button onClick={userChnageHandler}>Users</button>
+      <button onClick={commentsChangeHandler}>Comments</button>
+      <h1>{resourstype}</h1>
+      {data.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
+    </div>
+  );
+};
 
 export default App;
